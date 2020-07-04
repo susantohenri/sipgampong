ca84f258444e64be6a5ef35eaa281ab36aebc4f5
Henri Susanto <henry.dinus@gmail.com>
2020-07-04 09:11:53 +0700

    support file upload

--- application/views/form.php
+++ application/views/form.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/select2.min.css') ?>">
 <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/bootstrap-datepicker.css') ?>">
-<form action="<?= site_url($current['controller']) ?>" method="POST" class="main-form col-sm-12">
+<form enctype='multipart/form-data' action="<?= site_url($current['controller']) ?>" method="POST" class="main-form col-sm-12">
   <div class="card card-primary card-outline">
     <div class="card-header text-right">
       <?php if ((empty($uuid) && in_array("create_{$current['controller']}", $permission)) || (!empty($uuid) && in_array("update_{$current['controller']}", $permission))) : ?>
